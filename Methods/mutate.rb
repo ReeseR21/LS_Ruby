b6ccab1cc6889e49c760d17c8637db544c94859b
Nakkia R <nakkia.roberts@outlook.com>
2021-12-01 10:41:40 -0700

    Completing the Methods section.

---
+++ Methods/mutate.rb
@@ -0,0 +1,19 @@
+#Example of a method definition that mutates the caller.
+a = [1, 2, 3]
+
+def mutate(array)
+  array.pop
+end
+p "Before mutate method: #{a}"
+p mutate(a)
+p "After mutate method: #{a}"
+
+#Example of a method definition that does not mutate the caller.
+# a = [1, 2, 3]
+
+# def no_mutate(array)
+#   array.last
+# end
+# p "Before no_mutate method: #{a}"
+# p no_mutate(a)
+# p "After no_mutate method: #{a}"
---
+++ Methods/return.rb
@@ -0,0 +1,48 @@
+# def add_three(number)
+#   number + 3
+# end
+# returned_value = add_three(4)
+# puts returned_value
+
+# def add_three(number)
+#   return number + 3
+# end
+# returned_value = add_three(4)
+# puts returned_value
+
+# def add_three(number)
+#   return number + 3
+#   number + 4
+# end
+# returned_value = add_three(4)
+# puts returned_value
+
+# def just_assignment(number)
+#   foo = number + 3
+# end
+
+# def add_three(n)
+#   n + 3
+# end
+# add_three(5)
+# add_three(5).times { puts 'this should print 8 times'}
+
+# def add_three(n)
+#   puts n + 3
+# end
+# add_three(5).times { puts "will this work?"}
+# add_three(5)
+
+# def add_three(n)
+#   new_value = n + 3
+#   puts new_value
+#   new_value
+# end
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
