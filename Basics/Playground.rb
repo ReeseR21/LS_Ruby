9f7c750e04d6c6987148d3928ba8d7fd76add3cd
Nakkia R <nakkia.roberts@outlook.com>
2022-04-29 13:38:13 -0700

    Added Rbenv.

--- Basics/Playground.rb
+++ Basics/Playground.rb
@@ -105,4 +105,4 @@
 # end
 # p provide_something
 
-puts "John " + "Doe"
+# puts "John " + "Doe"
--- Methods/playground.rb
+++ Methods/playground.rb
@@ -1,8 +1,15 @@
-def scream(words)
-  words = words + "!!!!"
-  # return
-  puts words
-  return
-end
+# def scream(words)
+#   words = words + "!!!!"
+#   # return
+#   puts words
+#   return
+# end
 
-scream("Yippeee")
+# scream("Yippeee")
+
+def some_method(numbers)
+  number = 7
+end
+a = 5
+some_method(a)
+puts a
